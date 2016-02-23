59bb26cf1b0aaa1ebe6a84e2f84176121231fd30
William Johnson <wjohnson1000@gmail.com>
2016-02-23 11:34:00 -0700

    trying to incorporate mongo

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
 var Express = require('express')
   , app = Express()
-//  , MongoClient = require('mongodb').MongoClient
+  , MongoClient = require('mongodb').MongoClient
   , assert = require('assert')
   , PORT = process.env.PORT || 3000
 
-//var url = process.env.DATABASE_URI || 'mongodb://localhost:27017/galv-war-room';
-//MongoClient.connect(url, function(err, db) {
-//  assert.equal(null, err);
-//  console.log("Connected correctly to server.");
-//});
+var url = process.env.DATABASE_URI || 'mongodb://localhost:27017/galv-war-room';
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+});
 
 app.get('/', function(request, response){
   response.send("hit home route")
